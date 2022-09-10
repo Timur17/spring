8c0f77da817671c9bf204eedeeace64588c6f71f
TimurTS <for_work17@mail.ru>
2022-09-10 11:30:39 +0300

    repetition tasks on lesson

--- 2022-08/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonService.java
+++ 2022-08/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonService.java
@@ -2,6 +2,7 @@ package ru.otus.spring.service;
 
 import ru.otus.spring.domain.Person;
 
+
 public interface PersonService {
 
     Person getByName(String name);
--- 2022-08/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
+++ 2022-08/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
@@ -1,17 +1,26 @@
 package ru.otus.spring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import ru.otus.spring.dao.PersonDao;
 import ru.otus.spring.domain.Person;
 
+@Service
 public class PersonServiceImpl implements PersonService {
 
+    @Value("${greeting:Hello}")
+    private String greeting;
     private final PersonDao dao;
 
-    public PersonServiceImpl(PersonDao dao) {
+    @Autowired
+    public PersonServiceImpl(@Qualifier("personDaoSimple") PersonDao dao) {
         this.dao = dao;
     }
 
     public Person getByName(String name) {
+        System.out.println(greeting);
         return dao.findByName(name);
     }
 }

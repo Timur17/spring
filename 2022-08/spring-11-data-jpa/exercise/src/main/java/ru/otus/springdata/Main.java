287c624bea3e3caea37903a95916133e8572f5d3
TimurTS <for_work17@mail.ru>
2022-12-10 23:17:35 +0300

    commit class execises_2

--- 2022-08/spring-11-data-jpa/exercise/src/main/java/ru/otus/springdata/Main.java
+++ 2022-08/spring-11-data-jpa/exercise/src/main/java/ru/otus/springdata/Main.java
@@ -3,7 +3,10 @@ package ru.otus.springdata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-
+import ru.otus.springdata.domain.Email;
+import ru.otus.springdata.domain.Person;
+import ru.otus.springdata.repository.EmailRepository;
+import ru.otus.springdata.repository.PersonRepository;
 
 
 @SpringBootApplication
@@ -11,12 +14,28 @@ public class Main {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Main.class);
-        //PersonRepository personRepository = context.getBean(PersonRepository.class);
-        //EmailRepository emailRepository = context.getBean(EmailRepository.class);
+        PersonRepository personRepository = context.getBean(PersonRepository.class);
+        EmailRepository emailRepository = context.getBean(EmailRepository.class);
 
-        // personRepository.save(new Person("Александр Сергеевич Пушкин"));
-        // personRepository.save(new Person("Михаил Юрьевич Лермонтов"));
-        // personRepository.save(new Person("Михаил Сергеевич Горбачев"));
+        Email email = new Email("sashka@gmail.com");
+        emailRepository.save(email);
+
+
+         personRepository.save(new Person("Александр Сергеевич Пушкин", email));
+//         personRepository.save(new Person("Михаил Юрьевич Лермонтов", new Email("sashka@gmail.com")));
+//         personRepository.save(new Person("Михаил Сергеевич Горбачев", new Email("gorbatiy@gmail.com")));
+
+        System.out.println(personRepository.findAll());
+        System.out.println(emailRepository.findAll());
+        System.out.println(personRepository.findByEmailAddress("sashka@gmail.com"));
+
+        System.out.println(emailRepository.findByAddress("sashka@gmail.com"));
+
+        emailRepository.updateEmailById(1L, "sashka_17@gmail.com");
+
+        System.out.println(emailRepository.findAll());
+
+
     }
 
 
--- 2022-08/spring-11-data-jpa/exercise/src/main/java/ru/otus/springdata/repository/EmailRepository.java
+++ 2022-08/spring-11-data-jpa/exercise/src/main/java/ru/otus/springdata/repository/EmailRepository.java
@@ -1,4 +1,23 @@
 package ru.otus.springdata.repository;
 
-public interface EmailRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.springdata.domain.Email;
+
+import java.util.Optional;
+
+public interface EmailRepository extends JpaRepository<Email, Long> {
+
+    @Query("Select e from Email e where e.address = :address")
+    Optional<Email> findByAddress(@Param("address") String address);
+
+    @Query("update Email e set e.address = :address where e.id = :id")
+    @Modifying
+    @Transactional
+    void updateEmailById(@Param("id")Long id, @Param("address")String address);
+
+
 }
